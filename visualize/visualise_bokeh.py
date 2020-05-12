c97cd4417a89cf4e9d888ba6ddb58f88873bfefe
Kateřina Čížková <katterrina98@gmail.com>
2020-05-12 13:24:20 +0200

    removed unnecessary imports

--- visualize/visualise_bokeh.py
+++ visualize/visualise_bokeh.py
@@ -9,10 +9,10 @@ from json import JSONEncoder # right? (source: https://docs.python.org/3/library
 from parameters import ParameterSet
 
 from bokeh.plotting import figure, output_file, show
-from bokeh.layouts import layout, gridplot
+from bokeh.layouts import gridplot
 from bokeh.models import ColumnDataSource
 
-from numpy import array, reshape, resize, where
+from numpy import array, resize, where
 from math import ceil
 
 def show_sheets_plots(plots, plots_per_line):
@@ -22,6 +22,8 @@ def show_sheets_plots(plots, plots_per_line):
 	"""
 
 	height = ceil(plots.__len__() / plots_per_line) 
+
+	# maybe? (not very elegant)
 	graphs_grid = array(list(plots.values()))
 	graphs_grid.resize((height,plots_per_line))
 	graphs_grid = where(graphs_grid==0, None, graphs_grid) 
@@ -50,9 +52,11 @@ def get_plot(data_store, sheet):
 
 
 def __main__():
-	data_store = PickledDataStore(load=True, parameters=ParameterSet({'root_directory': '/home/katerina/matfyz/rocnikac/vzorove_mozaik/FeedForwardInhibition_student' ,'store_stimuli' : False}), replace=False)
 
-	output_file("testOutput.html") # todo
+	# todo ------
+	data_store = PickledDataStore(load=True, parameters=ParameterSet({'root_directory': '/home/katerina/matfyz/rocnikac/vzorove_mozaik/FeedForwardInhibition_student' ,'store_stimuli' : False}), replace=False)
+	output_file("testOutput.html")
+	#------------
 
 	graphs = {}
 
