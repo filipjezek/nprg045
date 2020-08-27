7b0f17f414a632ec42ee309652b6f09f1801786b
Kateřina Č <katterrina98@gmail.com>
2020-08-27 23:12:29 +0200

    creating multilayer networkx graph

--- test/test_visualize.py
+++ test/test_visualize.py
@@ -38,7 +38,8 @@ def test_add_sheet_nodes_to_empty_graph(mocker):
                              ((1,"SHEET"),{"coor":(2,2)}),
                              ((2,"SHEET"),{"coor":(3,1)})])
 
-    result = graph_functions.add_sheet_nodes_to_graph(nx.DiGraph(),None,"SHEET")
+    result = nx.DiGraph()
+    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
     
     assert expected.nodes == result.nodes
     
@@ -52,7 +53,8 @@ def test_add_sheet_nodes_to_graph_empty_sheet(mocker):
     expected = nx.DiGraph()
     expected.add_nodes_from([])
 
-    result = graph_functions.add_sheet_nodes_to_graph(nx.DiGraph(),None,"SHEET")
+    result = nx.DiGraph()
+    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
     
     assert expected.nodes == result.nodes
 
@@ -69,10 +71,9 @@ def test_add_sheet_nodes_same_node_index_different_sheets(mocker):
                              ((2,"SHEET"),{"coor":(3,1)}),
                              ((0,"SHEET_2"),{"coor":(2,1)})])
 
-    g = nx.DiGraph()
-    g.add_nodes_from([((0,"SHEET_2"),{"coor":(2,1)})])
-
-    result = graph_functions.add_sheet_nodes_to_graph(g,None,"SHEET")
+    result = nx.DiGraph()
+    result.add_nodes_from([((0,"SHEET_2"),{"coor":(2,1)})])
+    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
     
     assert expected.nodes == result.nodes
     
@@ -88,33 +89,34 @@ def test_add_sheet_nodes_to_empty_graph_coors(mocker):
                              ((1,"SHEET"),{"coor":(2,2)}),
                              ((2,"SHEET"),{"coor":(3,1)})])
 
-    result = graph_functions.add_sheet_nodes_to_graph(nx.DiGraph(),None,"SHEET")
+    result = nx.DiGraph()
+    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
     
     assert expected.nodes[(0,"SHEET")]["coor"] == result.nodes[(0,"SHEET")]["coor"]
     
 def test_add_connection_edges():
-    g = nx.DiGraph()
-    g.add_nodes_from([(0,"A"),(1,"A"),(0,"B")])
-    
-    expected = g.copy()
+    result = nx.DiGraph()
+    result.add_nodes_from([(0,"A"),(1,"A"),(0,"B")])
+   
+    expected = result.copy()
     expected.add_edges_from([((0,"A"),(0,"B"),{'weight':1, 'delay':1}),
                              ((1,"A"),(0,"B"),{'weight':1, 'delay':1})])
     
-    result = graph_functions.add_connection_edges(g, "A", "B", [(0,0,1),(1,0,1)], [(0,0,1),(1,0,1)])
+    graph_functions.add_connection_edges(result, "A", "B", [(0,0,1),(1,0,1)], [(0,0,1),(1,0,1)])
     
     assert expected.out_edges == result.out_edges
     
 def test_add_connection_edges_attributes():
-    g = nx.DiGraph()
-    g.add_nodes_from([(0,"A"),(1,"A"),(0,"B")])
+    result = nx.DiGraph()
+    result.add_nodes_from([(0,"A"),(1,"A"),(0,"B")])
     
-    expected = g.copy()
-    expected.add_edges_from([((0,"A"),(0,"B"),{'weight':1, 'delay':1}),
-                             ((1,"A"),(0,"B"),{'weight':1, 'delay':1})])
+    expected = result.copy()
+    expected.add_edges_from([((0,"A"),(0,"B"),{'weight':1, 'delay':1})])
     
-    result = graph_functions.add_connection_edges(g, "A", "B", [(0,0,1),(1,0,1)], [(0,0,1),(1,0,1)])
+    graph_functions.add_connection_edges(result, "A", "B", [(0,0,1)], [(0,0,1)])
     
-    expected_dict = expected.out_edges[((1,"A"),(0,"B"))]
-    result_dict = result.out_edges[((1,"A"),(0,"B"))]
+    expected_dict = expected.out_edges[((0,"A"),(0,"B"))]
+    result_dict = result.out_edges[((0,"A"),(0,"B"))]
     
     assert expected_dict == result_dict
+    
--- visualize/graph_functions.py
+++ visualize/graph_functions.py
@@ -11,18 +11,18 @@ from parameters import UniformDist
 from json import JSONEncoder
 from parameters import ParameterSet
 
-from numpy import array, resize, where, arange
-
+from numpy import arange
+from bokeh.plotting import from_networkx
 import networkx as nx
 
 
 def get_datastore(path_to_datastore):
-	data_store = PickledDataStore(
+	datastore = PickledDataStore(
 					load=True,
 					parameters=ParameterSet(
 						{'root_directory': path_to_datastore ,'store_stimuli' : False}),
 					replace=False)
-	return data_store
+	return datastore
 
 def get_neuron_positions_on_sheet(datastore, sheet):
 	positions = {
@@ -34,12 +34,10 @@ def get_neuron_positions_on_sheet(datastore, sheet):
 
 def add_connection_edges(G, source, target, conn_weights, conn_delays):
 	connection_edges = [((i,source),(j,target),{'weight':w, 'delay':d})
-                    	for ((i,j,w),(_,_,d))
+						for ((i,j,w),(_,_,d))
 						in zip(conn_weights,conn_delays)]
 
 	G.add_edges_from(connection_edges)
- 
-	return G
 
 def add_sheet_nodes_to_graph(G,datastore,sheet):
 	positions = get_neuron_positions_on_sheet(datastore,sheet)
@@ -51,8 +49,21 @@ def add_sheet_nodes_to_graph(G,datastore,sheet):
 						in node_indices]
 		
 	G.add_nodes_from(nodes_with_sheet)
-		
-	return G
 
-def add_edges_to_graph(G,datastore):
-	raise NotImplementedError()
+def create_neuron_connections_graph(datastore):
+	G = nx.DiGraph()
+
+	for sheet in datastore.sheets():
+		add_sheet_nodes_to_graph(G,datastore,sheet)
+
+	connections = datastore.get_analysis_result(identifier='Connections')
+
+	for conn in connections:
+		c_weights = conn.weights
+		c_delays = conn.delays
+		add_connection_edges(G, conn.source_name, conn.target_name, c_weights, c_delays)
+
+def graph_to_show(G,sheets,selection=None,edges_in=False):
+		if selection == None:
+			for sheet in sheets:
+				graph = from_networkx(G(sheet=sheet))
