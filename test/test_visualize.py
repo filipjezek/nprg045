1b800cb574919d507b4b5dc8ecd408c10bb87b03
Kateřina Č <katterrina98@gmail.com>
2020-08-29 18:21:23 +0200

    numeric node indices, tests modified

--- test/test_visualize.py
+++ test/test_visualize.py
@@ -26,23 +26,6 @@ from pytest_mock import mocker
 from visualize import graph_functions
 # ---
 
-def test_add_sheet_nodes_to_empty_graph(mocker):
-    mocker.patch(
-        'visualize.graph_functions.get_neuron_positions_on_sheet',
-        return_value = {'x': [ 2.0, 2.0, 3.0],
-                        'y': [ 1.0, 2.0, 1.0]}
-    )
-
-    expected = nx.DiGraph()
-    expected.add_nodes_from([((0,"SHEET"),{"coor":(2,1)}),
-                             ((1,"SHEET"),{"coor":(2,2)}),
-                             ((2,"SHEET"),{"coor":(3,1)})])
-
-    result = nx.DiGraph()
-    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
-    
-    assert expected.nodes == result.nodes
-    
 def test_add_sheet_nodes_to_graph_empty_sheet(mocker):
     mocker.patch(
         'visualize.graph_functions.get_neuron_positions_on_sheet',
@@ -54,11 +37,11 @@ def test_add_sheet_nodes_to_graph_empty_sheet(mocker):
     expected.add_nodes_from([])
 
     result = nx.DiGraph()
-    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
+    graph_functions.add_sheet_nodes_to_graph(result,None,0,"SHEET")
     
     assert expected.nodes == result.nodes
 
-def test_add_sheet_nodes_same_node_index_different_sheets(mocker):
+def test_add_sheet_nodes_to_empty_graph(mocker):
     mocker.patch(
         'visualize.graph_functions.get_neuron_positions_on_sheet',
         return_value = {'x': [ 2.0, 2.0, 3.0],
@@ -66,14 +49,32 @@ def test_add_sheet_nodes_same_node_index_different_sheets(mocker):
     )
 
     expected = nx.DiGraph()
-    expected.add_nodes_from([((0,"SHEET"),{"coor":(2,1)}),
-                             ((1,"SHEET"),{"coor":(2,2)}),
-                             ((2,"SHEET"),{"coor":(3,1)}),
-                             ((0,"SHEET_2"),{"coor":(2,1)})])
+    expected.add_nodes_from([(0,{"sheet":"A","coor":(2,1)}),
+                             (1,{"sheet":"A","coor":(2,2)}),
+                             (2,{"sheet":"A","coor":(3,1)})])
 
     result = nx.DiGraph()
-    result.add_nodes_from([((0,"SHEET_2"),{"coor":(2,1)})])
-    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
+    graph_functions.add_sheet_nodes_to_graph(result,None,0,"A")
+    
+    assert expected.nodes == result.nodes
+
+def test_add_sheet_nodes_to_non_empty_graph(mocker):
+    mocker.patch(
+        'visualize.graph_functions.get_neuron_positions_on_sheet',
+        return_value = {'x': [ 2.0, 2.0, 3.0],
+                        'y': [ 1.0, 2.0, 1.0]}
+    )
+
+    expected = nx.DiGraph()
+    expected.add_nodes_from([(0,{"sheet":"A","coor":(2,1)}),
+                             (1,{"sheet":"B","coor":(2,1)}),
+                             (2,{"sheet":"B","coor":(2,2)}),
+                             (3,{"sheet":"B","coor":(3,1)})])
+
+    result = nx.DiGraph()
+    result.add_nodes_from([(0,{"sheet":"A","coor":(2,1)})])
+    n = result.number_of_nodes()
+    graph_functions.add_sheet_nodes_to_graph(result,None,n,"B")
     
     assert expected.nodes == result.nodes
     
@@ -85,38 +86,38 @@ def test_add_sheet_nodes_to_empty_graph_coors(mocker):
     )
 
     expected = nx.DiGraph()
-    expected.add_nodes_from([((0,"SHEET"),{"coor":(2,1)}),
-                             ((1,"SHEET"),{"coor":(2,2)}),
-                             ((2,"SHEET"),{"coor":(3,1)})])
+    expected.add_nodes_from([(0,{"sheet":"A","coor":(2,1)}),
+                             (1,{"sheet":"A","coor":(2,2)}),
+                             (2,{"sheet":"A","coor":(3,1)})])
 
     result = nx.DiGraph()
-    graph_functions.add_sheet_nodes_to_graph(result,None,"SHEET")
+    graph_functions.add_sheet_nodes_to_graph(result,None,0,"A")
     
-    assert expected.nodes[(0,"SHEET")]["coor"] == result.nodes[(0,"SHEET")]["coor"]
+    assert expected.nodes[0]["coor"] == result.nodes[0]["coor"]
     
 def test_add_connection_edges():
     result = nx.DiGraph()
-    result.add_nodes_from([(0,"A"),(1,"A"),(0,"B")])
+    result.add_nodes_from([0,1,2])
    
     expected = result.copy()
-    expected.add_edges_from([((0,"A"),(0,"B"),{'weight':1, 'delay':1}),
-                             ((1,"A"),(0,"B"),{'weight':1, 'delay':1})])
+    expected.add_edges_from([(0,2,{'weight':1, 'delay':1}),
+                             (1,2,{'weight':1, 'delay':1})])
     
-    graph_functions.add_connection_edges(result, "A", "B", [(0,0,1),(1,0,1)], [(0,0,1),(1,0,1)])
+    graph_functions.add_connection_edges(result, 0, 2, [(0,0,1),(1,0,1)], [(0,0,1),(1,0,1)])
     
     assert expected.out_edges == result.out_edges
     
 def test_add_connection_edges_attributes():
     result = nx.DiGraph()
-    result.add_nodes_from([(0,"A"),(1,"A"),(0,"B")])
-    
+    result.add_nodes_from([0,1,2])
+   
     expected = result.copy()
-    expected.add_edges_from([((0,"A"),(0,"B"),{'weight':1, 'delay':1})])
+    expected.add_edges_from([(0,2,{'weight':1, 'delay':1})])
     
-    graph_functions.add_connection_edges(result, "A", "B", [(0,0,1)], [(0,0,1)])
+    graph_functions.add_connection_edges(result, 0, 2, [(0,0,1),(1,0,1)], [(0,0,1),(1,0,1)])
     
-    expected_dict = expected.out_edges[((0,"A"),(0,"B"))]
-    result_dict = result.out_edges[((0,"A"),(0,"B"))]
+    expected_dict = expected.out_edges[(0,2)]
+    result_dict = result.out_edges[(0,2)]
     
     assert expected_dict == result_dict
     
--- visualize/graph_functions.py
+++ visualize/graph_functions.py
@@ -12,7 +12,7 @@ from json import JSONEncoder
 from parameters import ParameterSet
 
 from numpy import arange
-from bokeh.plotting import from_networkx
+from bokeh.plotting import figure, curdoc, from_networkx
 import networkx as nx
 
 
@@ -33,18 +33,18 @@ def get_neuron_positions_on_sheet(datastore, sheet):
 	return positions
 
 def add_connection_edges(G, source, target, conn_weights, conn_delays):
-	connection_edges = [((i,source),(j,target),{'weight':w, 'delay':d})
+	connection_edges = [(i+source,j+target,{'weight':w, 'delay':d})
 						for ((i,j,w),(_,_,d))
 						in zip(conn_weights,conn_delays)]
 
 	G.add_edges_from(connection_edges)
 
-def add_sheet_nodes_to_graph(G,datastore,sheet):
+def add_sheet_nodes_to_graph(G,datastore,n,sheet):
 	positions = get_neuron_positions_on_sheet(datastore,sheet)
 		
 	node_indices = arange(len(positions['x']))
-	nodes_with_sheet = [((i,sheet),
-							{"coor":(positions['x'][i],positions['y'][i])})
+	nodes_with_sheet = [(i+n,
+							{"sheet":sheet,"coor":(positions['x'][i],positions['y'][i])})
 						for i
 						in node_indices]
 		
@@ -53,17 +53,29 @@ def add_sheet_nodes_to_graph(G,datastore,sheet):
 def create_neuron_connections_graph(datastore):
 	G = nx.DiGraph()
 
+	sheet_nodes_start_number = {}
+
 	for sheet in datastore.sheets():
-		add_sheet_nodes_to_graph(G,datastore,sheet)
+		n = G.number_of_nodes() # maybe do it faster?
+		sheet_nodes_start_number[sheet] = n
+		add_sheet_nodes_to_graph(G,datastore,n,sheet)
 
 	connections = datastore.get_analysis_result(identifier='Connections')
 
 	for conn in connections:
 		c_weights = conn.weights
 		c_delays = conn.delays
-		add_connection_edges(G, conn.source_name, conn.target_name, c_weights, c_delays)
+		add_connection_edges(G,
+							sheet_nodes_start_number[conn.source_name],
+							sheet_nodes_start_number[conn.target_name],
+							c_weights, c_delays)
 
-def graph_to_show(G,sheets,selection=None,edges_in=False):
-		if selection == None:
-			for sheet in sheets:
-				graph = from_networkx(G(sheet=sheet))
+
+def sheet_graph_to_show(G,sheet,selection=None,edges_in=False):
+	if selection==None: # works (shows also edges)
+		sheet_nodes = [n for n,attr in G.nodes(data=True) if attr["sheet"]==sheet]
+		sheet_subgraph = G.subgraph(sheet_nodes)
+		layout = {n:sheet_subgraph.nodes[n]["coor"] for n in sheet_subgraph.nodes()}
+		graph = from_networkx(sheet_subgraph,layout)
+
+	return graph
