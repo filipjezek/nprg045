f32e0c89cfe9d9c426ce339d2ab60a646b6c9b20
Kateřina Č <katterrina98@gmail.com>
2020-08-30 15:10:58 +0200

    basic plots again, without holoviews

--- visualize/graph_functions.py
+++ visualize/graph_functions.py
@@ -12,7 +12,9 @@ from json import JSONEncoder
 from parameters import ParameterSet
 
 from numpy import arange
-from bokeh.plotting import figure, curdoc, from_networkx
+from bokeh.plotting import figure, curdoc, from_networkx, show
+from bokeh.models import (BoxZoomTool, Circle, HoverTool,
+                          MultiLine, Plot, Range1d, ResetTool,)
 import networkx as nx
 
 
@@ -42,13 +44,12 @@ def add_connection_edges(G, source, target, conn_weights, conn_delays):
 def add_sheet_nodes_to_graph(G,datastore,n,sheet):
 	positions = get_neuron_positions_on_sheet(datastore,sheet)
 		
-	node_indices = arange(len(positions['x']))
-	nodes_with_sheet = [(i+n,
-							{"sheet":sheet,"coor":(positions['x'][i],positions['y'][i])})
+	node_indices = [i for i in range(len(positions['x']))]
+	nodes_with_sheet = [(i+n,{"coor":(positions['x'][i],positions['y'][i])})
 						for i
 						in node_indices]
 		
-	G.add_nodes_from(nodes_with_sheet)
+	G.add_nodes_from(nodes_with_sheet,sheet=sheet)
 
 def create_neuron_connections_graph(datastore):
 	G = nx.DiGraph()
@@ -70,12 +71,44 @@ def create_neuron_connections_graph(datastore):
 							sheet_nodes_start_number[conn.target_name],
 							c_weights, c_delays)
 
+	return G
 
-def sheet_graph_to_show(G,sheet,selection=None,edges_in=False):
-	if selection==None: # works (shows also edges)
-		sheet_nodes = [n for n,attr in G.nodes(data=True) if attr["sheet"]==sheet]
-		sheet_subgraph = G.subgraph(sheet_nodes)
-		layout = {n:sheet_subgraph.nodes[n]["coor"] for n in sheet_subgraph.nodes()}
-		graph = from_networkx(sheet_subgraph,layout)
+
+def sheet_graph_to_show(G,sheet):
+	
+	sheet_nodes = [n for n,attr in G.nodes(data=True) if attr["sheet"]==sheet]
+	sheet_subgraph = G.subgraph(sheet_nodes)
+	layout = {n:sheet_subgraph.nodes[n]["coor"] for n in sheet_subgraph.nodes()}
+	graph = from_networkx(sheet_subgraph,layout)
 
 	return graph
+
+def graph_according_selection(G,selection=[],edges_in=False):
+
+	selected_edges = []
+	nodes = G.nodes(data=True)
+ 
+	graph_to_show = nx.DiGraph()
+	graph_to_show.add_nodes_from(nodes, selected=0)
+	
+	for selected_node in selection:
+		graph_to_show.nodes[selected_node]["selected"] = 1
+
+		neighbors = []
+		if edges_in==False:
+			neighbors = G.successors(selected_node)
+		else:
+			neighbors = G.predecessors(selected_node)
+
+		for n in neighbors:
+			if graph_to_show.nodes[n]["selected"] == 0:
+				graph_to_show.nodes[n]["selected"] = 2
+			if nodes[n]["sheet"]==nodes[selected_node]["sheet"]:
+				if edges_in==False:
+					selected_edges.append((selected_node,n,G[selected_node][n]))
+				else:
+					selected_edges.append((n,selected_node,G[n][selected_node]))
+
+	graph_to_show.add_edges_from(selected_edges)
+
+	return graph_to_show
--- visualize/main.py
+++ visualize/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 
+# mozaik
 from pyNN.random import RandomDistribution
 from mozaik.tools.distribution_parametrization import MozaikExtendedParameterSet, load_parameters, PyNNDistribution
 from mozaik.storage.datastore import Hdf5DataStore,PickledDataStore
@@ -11,105 +12,38 @@ from parameters import UniformDist
 from json import JSONEncoder
 from parameters import ParameterSet
 
-from bokeh.plotting import figure, curdoc
-from bokeh.layouts import gridplot
-from bokeh.models import ColumnDataSource, LassoSelectTool, TapTool
-
-import holoviews as hv 
-import datashader as ds
-from holoviews.operation.datashader import datashade, shade, dynspread, rasterize, bundle_graph
+# visualization
+from bokeh.plotting import figure, curdoc, show
+from bokeh.layouts import gridplot, layout
+from bokeh.models import ToolbarBox, Toolbar, LassoSelectTool, WheelZoomTool,ResetTool, PanTool,HoverTool, Plot
 
 from numpy import array, resize, where, arange
-from math import ceil
 
 import networkx as nx
 
-from .graph_functions import *
+from graph_functions import *
 
-#------------------------------------------------new
-
-def add_sheet_nodes_to_graph(sheet):
-    
-	positions = {
-		'x' : data_store.get_neuron_positions()[sheet][0],
-		'y' : data_store.get_neuron_positions()[sheet][1]
-	}
-
-	return positions
-
-#---------------------------------------------------
-
-hv.extension('bokeh')
-
-defaults = dict(width=800, height=800)
- 
-hv.opts.defaults(
-    	hv.opts.EdgePaths(**defaults),
-    	hv.opts.Graph(**defaults),
-    	hv.opts.Nodes(**defaults)
-    )
-
-
-
-def connections_inside_layer(connections,nodes):
-	c_source = [int(i) for (i,j,weight) in connections]
-	c_target = [int(j) for (i,j,weight) in connections]
-	edge_labels = [x for (i,j,x) in connections]
-	g = hv.Graph(((c_source, c_target, edge_labels), nodes), vdims='weight')
-	return g
-
-def get_plot(data_store, sheet):
-	"""
-	Returns plot for the sheet.
-	"""
-
-	positions = {
-		'x' : data_store.get_neuron_positions()[sheet][0],
-		'y' : data_store.get_neuron_positions()[sheet][1]
-	}
-
-	#-----
-
-
-	node_indices = arange(len(positions['x']))
-	nodes = hv.Nodes((positions['x'],positions['y'], node_indices), )
- 
- 
-	connections = data_store.get_analysis_result(identifier='Connections')
-	print(connections)
-	graph = hv.Graph((([],[],[]), nodes), )
-
-	print(connections)
-
-	for conn in connections:
-		print(conn)
-		if (conn.source_name == sheet and conn.target_name == sheet):
-			graph = connections_inside_layer(conn.weights,nodes)
-
-	#-----
-
-	return graph.relabel(sheet)
-
+tools = [LassoSelectTool(), ResetTool(), WheelZoomTool(), PanTool(),HoverTool()]
     
 path_to_data = sys.argv[1]
 # for me: /home/katterrina/matfyz/rocnikac/vzorove_mozaik/FeedForwardInhibition_student
-data_store = get_datastore(path_to_data)
+datastore = get_datastore(path_to_data)
+neuron_connections_graph = create_neuron_connections_graph(datastore)
 
-graphs = []
+g_to_show = graph_according_selection(neuron_connections_graph)
 
-for sheet in data_store.sheets():
-	g=get_plot(data_store,sheet)
-	g.opts(edge_line_width=1, node_size=5)
-	g.opts(hv.opts.Graph(inspection_policy='nodes', tools=['hover', 'lasso_select'],
-			edge_hover_line_color='green'))
-	graphs.append(g)
+plots=[]
+
+for sheet in datastore.sheets():
+    sheet_graph_renderer = sheet_graph_to_show(g_to_show,sheet)
+    
+    sheet_graph_plot = figure(title=sheet,
+				  x_range=(-1,1), y_range=(-1,1),
+	              tools="lasso_select,pan,wheel_zoom,reset,hover")
+    sheet_graph_plot.renderers.append(sheet_graph_renderer)
+    plots.append(sheet_graph_plot )
 
 
+layout = gridplot(plots, ncols=2,plot_width=800, plot_height=800)
 
-
-layout = hv.Layout(graphs).cols(2)
-layout.opts(shared_axes=False)
-
-# put the button and plot in a layout and add to the document
-#curdoc().add_root(show_graphs(graphs,2))
-hv.renderer('bokeh').server_doc(layout)
+curdoc().add_root(layout)
