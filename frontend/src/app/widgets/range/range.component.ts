5eefade8ff1752a7cf495d8e1adf2c21865b3579
Filip Ježek <filip.jezek@gmail.com>
2022-07-20 09:49:11 +0200

    add range widget

---
+++ frontend/src/app/widgets/range/range.component.ts
@@ -0,0 +1,63 @@
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import {
+  FormControl,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
+
+@Component({
+  selector: 'mozaik-range',
+  templateUrl: './range.component.html',
+  styleUrls: ['./range.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RangeComponent),
+      multi: true,
+    },
+  ],
+})
+export class RangeComponent implements OnInit, ControlValueAccessor {
+  @Input() step: number | 'any';
+  @Input() min: number;
+  @Input() max: number;
+  @Input() get value(): number {
+    return this.control.value;
+  }
+  set value(v: number) {
+    this.control.setValue(v);
+  }
+  control = new FormControl();
+  disabled = false;
+  private onTouchedCb: () => void;
+  private onChangeCb: (val: number) => void;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  writeValue(v: number) {
+    this.value = v;
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCb = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouchedCb = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+    this.control[isDisabled ? 'disable' : 'enable']();
+  }
+
+  onChange(v: number) {
+    this.onChangeCb(v);
+  }
+
+  onTouched() {
+    this.onTouchedCb();
+  }
+}
--- frontend/src/app/widgets/widgets.module.ts
+++ frontend/src/app/widgets/widgets.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonRadioComponent } from './button-radio/button-radio.component';
 import { AcronymPipe } from './acronym.pipe';
+import { RangeComponent } from './range/range.component';
+import { KeyboardClickDirective } from './directives/keyboard-click.directive';
 
 @NgModule({
-  declarations: [ButtonRadioComponent, AcronymPipe],
+  declarations: [
+    ButtonRadioComponent,
+    AcronymPipe,
+    RangeComponent,
+    KeyboardClickDirective,
+  ],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [ButtonRadioComponent, AcronymPipe],
+  exports: [
+    ButtonRadioComponent,
+    AcronymPipe,
+    RangeComponent,
+    KeyboardClickDirective,
+  ],
 })
 export class WidgetsModule {}
