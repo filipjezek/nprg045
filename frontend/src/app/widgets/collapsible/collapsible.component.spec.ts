ed69b66985aa77ad009fc92961632925ddcb313a
Filip Ježek <filip.jezek@gmail.com>
2023-06-18 22:09:47 +0200

    update tests for collapsible component

--- frontend/src/app/widgets/collapsible/collapsible.component.spec.ts
+++ frontend/src/app/widgets/collapsible/collapsible.component.spec.ts
@@ -1,23 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 
 import { CollapsibleComponent } from './collapsible.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+
+@Component({
+  template: `<mozaik-collapsible>
+    <span slot="header" class="test-header">heading</span>
+    <p>test content</p>
+  </mozaik-collapsible>`,
+})
+class Container {}
 
 describe('CollapsibleComponent', () => {
-  let component: CollapsibleComponent;
-  let fixture: ComponentFixture<CollapsibleComponent>;
+  let component: Container;
+  let fixture: ComponentFixture<Container>;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CollapsibleComponent ]
-    })
-    .compileComponents();
+      declarations: [CollapsibleComponent, Container],
+      imports: [NoopAnimationsModule],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(CollapsibleComponent);
+    fixture = TestBed.createComponent(Container);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    el = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should hide content', fakeAsync(() => {
+    expect(el.querySelector('p')).toBeNull();
+    el.querySelector('header').click();
+    fixture.detectChanges();
+    tick(); // because of animations
+    const p = el.querySelector('p');
+    expect(p.textContent).toBe('test content');
+    el.querySelector('header').click();
+    fixture.detectChanges();
+    tick();
+    expect(el.querySelector('p')).toBeNull();
+  }));
+
+  it('should display header', () => {
+    expect(el.querySelector('header .test-header').textContent).toBe('heading');
   });
 });
