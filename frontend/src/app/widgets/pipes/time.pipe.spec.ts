a9f78b6fcd68b95a657d747ebcc504bf2b98d444
Filip Ježek <filip.jezek@gmail.com>
2023-06-19 17:35:23 +0200

    add property bag tests

--- frontend/src/app/widgets/pipes/time.pipe.spec.ts
+++ frontend/src/app/widgets/pipes/time.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { TimePipe } from './time.pipe';
 
-fdescribe('TimePipe', () => {
+describe('TimePipe', () => {
   it('create an instance', () => {
     const pipe = new TimePipe();
     expect(pipe).toBeTruthy();
--- frontend/src/app/widgets/property-bag/property-bag.component.spec.ts
+++ frontend/src/app/widgets/property-bag/property-bag.component.spec.ts
@@ -1,23 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PropertyBagComponent } from './property-bag.component';
+import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { PurefnPipe } from '../pipes/purefn.pipe';
+
+@Component({
+  template: `
+    <mozaik-property-bag
+      [formControl]="control"
+      [bag]="items"
+    ></mozaik-property-bag>
+  `,
+})
+class Container {
+  control = new FormControl();
+  items = {
+    foo: 'bar',
+    baz: 'cat',
+  };
+}
 
 describe('PropertyBagComponent', () => {
-  let component: PropertyBagComponent;
-  let fixture: ComponentFixture<PropertyBagComponent>;
+  let component: Container;
+  let fixture: ComponentFixture<Container>;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PropertyBagComponent ]
-    })
-    .compileComponents();
+      declarations: [PropertyBagComponent, Container, PurefnPipe],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(PropertyBagComponent);
+    fixture = TestBed.createComponent(Container);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    el = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should work as a radio input', () => {
+    expect(component.control.value).toBeNull();
+    const items = Array.from(el.querySelectorAll<HTMLElement>('.item'));
+
+    items.find((it) => it.textContent.trim() == 'foo').click();
+    fixture.detectChanges();
+    expect(component.control.value).toBe('foo');
+    items.find((it) => it.textContent.trim() == 'baz').click();
+    fixture.detectChanges();
+    expect(component.control.value).toBe('baz');
   });
 });
