43543bfe01f73c222127649c3470947d823db62e
Filip Ježek <filip.jezek@gmail.com>
2023-06-19 18:57:42 +0200

    add range component tests

--- frontend/src/app/widgets/range/range.component.spec.ts
+++ frontend/src/app/widgets/range/range.component.spec.ts
@@ -1,7 +1,8 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RangeComponent } from './range.component';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Pipe({ name: 'scientific' })
 class FakeScientificPipe implements PipeTransform {
@@ -10,23 +11,50 @@ class FakeScientificPipe implements PipeTransform {
   }
 }
 
+@Component({
+  template: `
+    <mozaik-range [min]="10" [max]="100" [formControl]="control"></mozaik-range>
+  `,
+})
+class Container {
+  control = new FormControl();
+}
+
 describe('RangeComponent', () => {
-  let component: RangeComponent;
-  let fixture: ComponentFixture<RangeComponent>;
+  let component: Container;
+  let fixture: ComponentFixture<Container>;
+  let el: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RangeComponent, FakeScientificPipe],
+      declarations: [RangeComponent, FakeScientificPipe, Container],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RangeComponent);
+    fixture = TestBed.createComponent(Container);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    el = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change value with range input', () => {
+    const inp = el.querySelector<HTMLInputElement>('[type=range]');
+    inp.value = '20';
+    inp.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.control.value).toBe(20);
+  });
+  it('should change value with number input', () => {
+    const inp = el.querySelector<HTMLInputElement>('[type=number]');
+    inp.value = '30';
+    inp.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(component.control.value).toBe(30);
   });
 });
