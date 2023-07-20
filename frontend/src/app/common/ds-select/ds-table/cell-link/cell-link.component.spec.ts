89b97eabae14430fc12d16bbf35a4e952ddfe6e0
Filip Ježek <filip.jezek@gmail.com>
2023-07-20 10:51:50 +0200

    add link cell component tests

--- frontend/src/app/common/ds-select/ds-table/cell-link/cell-link.component.spec.ts
+++ frontend/src/app/common/ds-select/ds-table/cell-link/cell-link.component.spec.ts
@@ -1,16 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CellLinkComponent } from './cell-link.component';
+import { Store } from '@ngrx/store';
+import { StoreStub } from 'src/app/testing/store.stub';
+import { DSCELL_VAL } from '../cell-generic/cell-generic.component';
+import { LinkWrapper } from '../../sql/user-sql-functions/make-link';
+import { RouterLinkStub } from 'src/app/testing/routerlink.stub';
+import { routerSelectors } from 'src/app/store/selectors/router.selectors';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterStub } from 'src/app/testing/router.stub';
 
 describe('CellLinkComponent', () => {
   let component: CellLinkComponent;
   let fixture: ComponentFixture<CellLinkComponent>;
 
   beforeEach(async () => {
+    spyOn(routerSelectors, 'selectRouteParams').and.returnValue({
+      ready: '1,2,3',
+      viewing: '1',
+    });
+    spyOn(routerSelectors, 'selectRouteParam').and.returnValue(
+      (() => 'testpath') as any
+    );
     await TestBed.configureTestingModule({
-      declarations: [ CellLinkComponent ]
-    })
-    .compileComponents();
+      declarations: [CellLinkComponent, RouterLinkStub],
+      providers: [
+        { provide: Store, useClass: StoreStub },
+        { provide: DSCELL_VAL, useValue: new LinkWrapper(12) },
+        { provide: Router, useClass: RouterStub },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CellLinkComponent);
     component = fixture.componentInstance;
@@ -19,5 +39,35 @@ describe('CellLinkComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create correct link', () => {
+    const routerlink = fixture.debugElement
+      .query(By.directive(RouterLinkStub))
+      .injector.get(RouterLinkStub);
+    expect(routerlink.routerLink).toEqual([
+      'datastore',
+      'testpath',
+      'inspect',
+      {
+        ready: ['1', '2', '3', '12'],
+        viewing: ['12'],
+      },
+    ]);
+  });
+
+  it('should open in background on middle click', () => {
+    const router: RouterStub = TestBed.inject(Router) as any;
+    const link: HTMLElement = fixture.nativeElement.querySelector('a');
+    link.dispatchEvent(new MouseEvent('auxclick', { button: 1 }));
+    expect(router.navigate).toHaveBeenCalledWith([
+      'datastore',
+      'testpath',
+      'inspect',
+      {
+        ready: ['1', '2', '3', '12'],
+        viewing: ['1'],
+      },
+    ]);
   });
 });
--- frontend/src/app/common/selected-neurons/add-neuron/add-neuron.component.spec.ts
+++ frontend/src/app/common/selected-neurons/add-neuron/add-neuron.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddNeuronComponent } from './add-neuron.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AddNeuronComponent', () => {
   let component: AddNeuronComponent;
@@ -8,7 +9,8 @@ describe('AddNeuronComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddNeuronComponent]
+      declarations: [AddNeuronComponent],
+      imports: [NoopAnimationsModule],
     });
     fixture = TestBed.createComponent(AddNeuronComponent);
     component = fixture.componentInstance;
